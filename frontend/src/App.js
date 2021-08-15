3c6dd65999de120ca559d4ebacfe8303459c8e68
Diego <diego.penaranda@ucb.edu.bo>
2021-08-15 12:16:53 -0400

    First functional commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    email: "",
+    password: "",
+    emailAnswer: "",
+    passwordAnswer: ""
+  }
+
+  componentDidMount() {
+    
+  }
+
+  handleChange = (event) => {
+    this.setState({email: event.target.value});
+  }
+
+  handleChange2 = (event) => {
+    this.setState({password: event.target.value});
+  }
+
+  handleSubmit = (event) => {
+    this.fetchAnswer();
+    event.preventDefault();
+  }
+  
+
+  fetchAnswer = async () => {
+    const headers = {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json'
+    }
+    const { email, password } = this.state;
+    const { data } = await axios.post(
+      `http://127.0.0.1:8080/api/verify_email`, { email, password }, { headers: headers }
+    );
+    const { emailAnswer, passwordAnswer } = data;
+    this.setState({emailAnswer})
+    this.setState({passwordAnswer})
+  }
+
+  render() {
+    const { email, password, emailAnswer, passwordAnswer } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <form onSubmit={this.handleSubmit}>
+          <label>
+            Email:
+            <input type="text" value={email} onChange={this.handleChange} />
+          </label>
+          <br/>
+          <label>
+            Password:
+            <input type="text" value={password} onChange={this.handleChange2} />
+          </label>
+          <br/>
+          <input type="submit" value="Submit" />
+        </form>
+        <h1>{emailAnswer}<br/>{passwordAnswer}</h1>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
