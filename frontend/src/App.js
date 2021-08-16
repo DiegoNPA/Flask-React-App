05dd98d4c186c44ca5c24933da4bc5bac2f76cad
Diego <diego.penaranda@ucb.edu.bo>
2021-08-15 22:18:47 -0400

    Created signup component and added formik

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import { Signup } from './Components/Signup';
 
 
 class App extends Component {
---
+++ frontend/src/Components/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Formik, Form } from 'formik';
+
+const Signup = () => {
+  return (
+    <Formik>
+      
+    </Formik>
+  )
+}
+
+export default Signup
